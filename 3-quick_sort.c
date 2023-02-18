cb1f32c74ed765fe542cf2d956561043902cd98e
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-18 09:39:01 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -64,3 +64,14 @@ void qs(int *array, ssize_t first, ssize_t last, int size)
 		qs(array, position + 1, last, size);
 	}
 }
+/**
+* quick_sort - prepare the terrain to quicksort algorithm
+* @array: array
+* @size: array size
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	qs(array, 0, size - 1, size);
+}
