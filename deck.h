e0633a084780b89e91638dd47ec9402c830dc945
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-20 13:26:23 -0800

    deck header file

---
+++ deck.h
@@ -0,0 +1,42 @@
+#ifndef DECK_H
+#define DECK_H
+
+/**
+* enum kind_e - card kind
+* @SPADE: spade kind
+* @HEART: heart kind
+* @CLUB: club kind
+* @DIAMOND: diamond kind
+*/
+typedef enum kind_e
+{
+	SPADE = 0
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+* struct card_s - playing card
+* @value: value of the card from Ace to King
+* @kind: card kind
+*/
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+* struct deck_node_s - deck of card
+* @card: pointer to the card of the node
+* @prev: pointer to the previous node of the list
+* @next: pointer to the next node of the list
+*/
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+#endif
