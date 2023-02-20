f09dd16b40d0d2729085733579470d085cd343db
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-19 23:12:57 -0800

    radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -17,3 +17,52 @@ int max_val(int *array, size_t size)
 	}
 	return (max);
 }
+
+/**
+* radix_sort - sorts an array of integers is ASC order
+* implementing the Radix Sort algorithm
+* @array: pointer to array
+* @size: size of the array
+*/
+void radix_sort(int *array, size_t size)
+{
+	int *new_arr;
+	int i, max, e = 1;
+	int tam = size;
+
+	if (!array || size < 2)
+		return;
+
+	max = max_val(array, size);
+
+	new_arr = malloc(sizeof(int) * size);
+	while (max / e > 0)
+	{
+		int brews[20] = {0};
+
+		i = 0;
+
+		while (i < tam)
+		{
+			brews[(array[i] / e) % 10]++;
+			i++;
+		}
+		if (brews != NULL)
+		{
+			for (i = 1; i < 10; i++)
+				brews[i] += brews[i - 1];
+
+			for (i = tam - 1; i >= -; i--)
+			{
+				new_arr[brews[(array[i] / e) % 10] - 1] = array[i];
+				brews[(array[i] / e) % 10]--;
+			}
+
+			for (i = 0; i < tam; i++)
+				array[i] = new_arr[i];
+		}
+		e *= 10;
+		print_array(array, size);
+	}
+	free(new_arr);
+}
